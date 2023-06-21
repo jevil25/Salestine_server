d5bcebcfc9c3b09aec14de197c807e56ce4e09f7
AaronNazareth <4nm20cs005@nmamit.in>
2023-06-21 21:07:56 +0530

    sync:with frontend

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 4000;
 const prisma = require("./utils/db/prisma");
 
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,7 +14,7 @@ app.use("/register", require("./Routes/register"));
 app.use("/login", require("./Routes/login"));
 app.use("/calls", require("./Routes/calls"));
 app.use("/googleAuth", require("./Routes/googleAuth"));
-app.use("/test", require("./Routes/test"));
+app.use("/validate", require("./Routes/validate"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
