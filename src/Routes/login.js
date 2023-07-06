65b8bff8fc4dfa6ae5d80a2a2e34fea40fb07591
AaronNazareth <4nm20cs005@nmamit.in>
2023-07-06 10:13:39 +0530

    fix:login api

--- src/Routes/login.js
+++ src/Routes/login.js
@@ -31,6 +31,10 @@ async function handler(req, res) {
       data: { token },
     });
 
+    if(user.role === "ADMIN"){
+      return res.status(400).json({ message: "Voice rec needed",user });
+    }
+
     if (user.voice_rec === "" && user.password_change === "" && user.googleCalendar === "") {
       return res.status(400).json({ message: "Voice rec ,password change and google calendar integration needed",user });
     }
