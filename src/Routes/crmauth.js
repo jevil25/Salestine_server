cfc743bcad336ba77facfa93d2d29dbbcbc80299
Parth Puranik <thosparth@gmail.com>
2023-07-13 13:26:36 +0530

    list opportunities backend addee

--- src/Routes/crmauth.js
+++ src/Routes/crmauth.js
@@ -2,7 +2,8 @@ const { APIDECK_API_KEY } = process.env;
 const { APIDECK_APP_ID } = process.env;
 
 async function handler(req, res) {
-  if (req.body.flag == "crmauth") {
+  const { flag } = req.body;
+  if (flag == "crmauth") {
     if (req.method != "POST") {
       res.status(400).json({ message: "Invalid method,only POST allowed" });
     }
@@ -16,9 +17,22 @@ async function handler(req, res) {
       },
     }).then((res) => res.json());
     res.status(200).json({ data: resp });
-  }
-  else{
-    res.status(400).json({error:"Invalid flag"})
+  } else if (flag == "listopp") {
+    if (req.method != "GET") {
+      res.status(400).json({ message: "Invalid method,only GET allowed" });
+    }
+    const data = await fetch("https://unify.apideck.com/crm/opportunities", {
+      headers: {
+        Authorization: "Bearer " + APIDECK_API_KEY,
+        "x-apideck-app-id": APIDECK_APP_ID,
+        "x-apideck-consumer-id": email,
+      },
+    }).then((res)=>res.json());
+    if(data.error == "Unauthorized"){
+      res.status(400).json({message:"Integrate your CRM"})
+    }
+  } else {
+    res.status(400).json({ error: "Invalid flag" });
   }
 }
 
