b80d69e2c60ea9f95923168722a4430c7b3a9110
AaronNazareth <4nm20cs005@nmamit.in>
2023-07-24 15:40:56 +0530

    feat:add feedback:

--- src/Routes/coaching.js
+++ src/Routes/coaching.js
@@ -40,4 +40,44 @@ router.post('/', async (req, res) => {
     return res.json({message: 'Success',status:true,data:company});
 });
 
+router.post('/addFeedback', async (req, res) => {
+    console.log(req.body);
+    const { email,userId,feedback } = req.body;
+    if(!email || !userId || !feedback){
+        return res.json({message: 'Email is required',status:false});
+    }
+    const user = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    });
+    if(!user){
+        return res.json({message: 'User not found',status:false});
+    }
+    const userF = await prisma.user.findUnique({
+        where: {
+            id: userId
+        }
+    });
+    if(!userF){
+        return res.json({message: 'User not found',status:false});
+    }
+    if(user.role !== 'ADMIN'){
+        return res.json({message: 'Only admin can add feedback',status:false});
+    }
+    if(user.companyId !== userF.companyId){
+        return res.json({message: 'Not admin of users company',status:false});
+    }
+    const feedbackCreate = await prisma.feedback.create({
+        data: {
+            userId: userId,
+            feedback: feedback
+        }
+    });
+    console.log(feedbackCreate);
+    if(!feedbackCreate){
+        return res.json({message: 'Feedback not added',status:false});
+    }
+    return res.json({message: 'Success',status:true});
+});
 module.exports = router;
